3d59fbec440580ce73c16a1b9ef79fc34cbacbd1
Oscar Quintanilla <oscar.david.quintanilla@gmail.com>
2019-09-23 12:40:23 -0600

    +Agregada la gestion de llaves para producción y desarrollo

---
+++ config/prod.ts
@@ -0,0 +1,8 @@
+// no hacer commit de esto
+export default {
+    user: "ElyteDbUser",
+    password: "Elyte210105$%",
+    server: "elyteclouddb.cftqqcal3x6b.us-east-2.rds.amazonaws.com",
+    database: "NetDataRCA"
+  };
+  
